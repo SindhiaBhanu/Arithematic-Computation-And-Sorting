25f1e29371d504458b8cebe289791ff7b8333ba3
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-10 22:14:51 +0530

    added code for storing results in dictionary for every computation

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A dictionary
 read -p "Enter first number  : " a
 read -p "Enter second number  : " b
 read -p "Enter third number  : " c
@@ -6,3 +7,7 @@ value1=$((a+b*c))
 value2=$((a*b+c))
 value3=$(( c+(a/b) ))
 value4=$(( (a%b)+c ))
+for((i=1;i<5;i++))
+do
+        dictionary[$i]="$((value$i))"
+done
